6cc360d012dc50076950ee322d5000344d7cf70f
Mr. Beat <mrbeat@mail.com>
2011-04-21 22:57:47 +0200

    Begun to figure out the ui. Added css file and the complete vx-lib (which seem a bit flawed now I have tried it, eg _.AC doesn't seem to work.)

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 
-
+function init() {
+    
+    var navigation = _.G('navigation');
+    var navigationitems = [
+        "Vem &auml;r Mr.Beat?",
+        "info/bokning/github",
+        "Freddots kalsongblogg"
+    ];
+    
+    var item;
+    
+    for (var i = 0, len = navigationitems.length; i < len; i++) {
+        item = document.createElement('div');
+        item.innerHTML = navigationitems[i];
+        item.className = 'navigationitem';
+        //_.AC(item, 'navigationitem');
+        navigation.appendChild(item);
+    }
+    
+}
 
